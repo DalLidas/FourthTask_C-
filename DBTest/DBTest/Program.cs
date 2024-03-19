95b167d6dfa2eb92039ccfa49a598fa346b38e9d
DalLidas <126471477+DalLidas@users.noreply.github.com>
2024-03-19 22:15:09 +0300

    Добавьте файлы проекта.

---
+++ DBTest/DBTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DBTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ FourthTask/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FourthTask
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
